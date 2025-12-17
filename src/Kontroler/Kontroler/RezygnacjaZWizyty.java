5e9ffcf790fbc2b75be66bcccd79818e7908d8bb
HOMA10111 <anton20051101@gmail.com>
2025-12-17 12:25:24 +0100

    Add files via upload

--- src/Kontroler/Kontroler/RezygnacjaZWizyty.java
+++ src/Kontroler/Kontroler/RezygnacjaZWizyty.java
@@ -4,33 +4,53 @@ import Model.Model.*;
 
 public class RezygnacjaZWizyty {
 
-	private IModel model;
-	private int idTerminu;
-	private string[] powodyRezygnacji;
-	private int pacjentID;
+    private IModel model;
+    private int idRezerwacji;
+    private int pacjentID;
+    private String powodRezygnacji;
 
-	/**
-	 * 
-	 * @param idTerminu
-	 * @param pacjentID
-	 */
-	public bool procesRezygnacji(int idTerminu, int pacjentID) {
-		// TODO - implement RezygnacjaZWizyty.procesRezygnacji
-		throw new UnsupportedOperationException();
-	}
+    public RezygnacjaZWizyty(IModel model) {
+        this.model = model;
+    }
 
-	/**
-	 * 
-	 * @param powod
-	 */
-	public void dodajPowod(string powod) {
-		// TODO - implement RezygnacjaZWizyty.dodajPowod
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Rozpoczęcie procesu rezygnacji
+     */
+    public boolean rozpocznijRezygnacje(int idRezerwacji, int pacjentID) {
+        System.out.println("KONTROLER: Rozpoczęcie rezygnacji z wizyty.");
 
-	public void zatwierdzRezygnacje() {
-		// TODO - implement RezygnacjaZWizyty.zatwierdzRezygnacje
-		throw new UnsupportedOperationException();
-	}
+        this.idRezerwacji = idRezerwacji;
+        this.pacjentID = pacjentID;
 
+        // W prawdziwym systemie tu byłoby sprawdzenie,
+        // czy rezerwacja należy do pacjenta
+        return true;
+    }
+
+    /**
+     * Podanie powodu rezygnacji
+     */
+    public void podajPowod(String powod) {
+        this.powodRezygnacji = powod;
+        System.out.println("KONTROLER: Podano powód rezygnacji: " + powod);
+    }
+
+    /**
+     * Zatwierdzenie rezygnacji
+     */
+    public void zatwierdzRezygnacje() {
+
+        if (powodRezygnacji == null || powodRezygnacji.isEmpty()) {
+            System.out.println("KONTROLER: Brak powodu rezygnacji!");
+            return;
+        }
+
+        // zapis powodu
+        model.zapiszPowodRezygnacji(idRezerwacji, powodRezygnacji);
+
+        // usunięcie rezerwacji
+        model.usunRezerwacjeZListyPacjenta(idRezerwacji, pacjentID);
+
+        System.out.println("KONTROLER: Rezygnacja zakończona sukcesem.");
+    }
 }
